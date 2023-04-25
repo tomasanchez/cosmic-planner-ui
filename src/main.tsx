7f18acfc4c766ae901de365beecf98daee2ea9e8
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-04-25 18:56:35 -0300

    test(dev): add jest
    configured with ts-jest

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './ts/App.tsx'
 import './index.css'
+import {createRoot} from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
 )
