bb86392a6274c0e3970b5f043a91abdeb4ddcef3
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-04-25 19:05:51 -0300

    Revert "Auth provider"

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -3,16 +3,13 @@ import {ThemeProvider} from "@mui/material";
 import {themeOptions} from "./themes";
 import {RouterProvider} from "react-router-dom";
 import router from "./Router";
-import AuthProvider from "../auth/AuthProvider";
 
 function App() {
 
     return (
-        <AuthProvider>
-            <ThemeProvider theme={themeOptions}>
-                <RouterProvider router={router}/>
-            </ThemeProvider>
-        </AuthProvider>
+        <ThemeProvider theme={themeOptions}>
+            <RouterProvider router={router}/>
+        </ThemeProvider>
     )
 }
 
