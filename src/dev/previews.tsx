d1509c435176444427219e6612de0d5be598638c
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-04-25 20:58:05 -0300

    feat(nav): add navbar
    - With auth profile

--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,12 +1,16 @@
 import {ComponentPreview, Previews} from "@react-buddy/ide-toolbox";
 import {PaletteTree} from "./palette";
 import {NavBar} from "../ts/components/header/NavBar";
+import Profile from "../ts/components/header/Profile";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
             <ComponentPreview path="/NavBar">
                 <NavBar/>
+            </ComponentPreview>
+            <ComponentPreview path="/Profile">
+                <Profile/>
             </ComponentPreview>
         </Previews>
     );
--- src/ts/components/header/NavBar.tsx
+++ src/ts/components/header/NavBar.tsx
@@ -1,30 +1,65 @@
-import {AppBar, Container, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, Container, Toolbar, Typography} from "@mui/material";
 import {Today} from "@mui/icons-material";
 import {deepPurple} from "@mui/material/colors";
+import Profile from "./Profile";
+import Button from "@mui/material/Button";
+import {useNavigate} from "react-router-dom";
+
+export interface NavBarNavigation {
+    name: string,
+
+    path: string
+
+}
+
 
 export const NavBar = () => {
+
+    const navigate = useNavigate()
+
+    const navigations: Array<NavBarNavigation> = [
+        {
+            name: "Product",
+            path: "/product"
+        }, {
+            name: "Pricing",
+            path: "/pricing"
+        }
+    ]
+
+    const onNavButton = (path: string) => {
+        navigate(path)
+    }
 
     return (
         <AppBar variant="outlined" sx={{background: "white", color: "black"}}
                 className={"user-select-none mb-5 w-100"}
         >
             <Container>
-                <Toolbar>
-                    <Today sx={{display: {xs: 'none', md: 'flex'}, mr: 1, color: deepPurple.A400}}/>
-                    <Typography variant="h5" noWrap component="a" href="/" sx={{
-                        flexGrow: 0.1,
-                        display: {xs: 'none', md: 'flex'},
-                        color: 'inherit',
-                        fontFamily: '"72 Black"',
-                        letterSpacing: '.1rem',
-                        textDecoration: 'none',
-                        ":hover": {
+                <Toolbar disableGutters>
+                    <Container sx={{display: {xs: 'flex', md: 'flex'}}}>
+                        <Today sx={{display: {xs: 'none', md: 'flex'}, mr: 1, color: deepPurple.A400}}/>
+                        <Typography variant="h5" noWrap component="a" href="/" sx={{
+                            flexGrow: 0.1,
+                            display: {xs: 'none', md: 'flex'},
+                            color: 'inherit',
+                            fontFamily: '"72 Black"',
+                            letterSpacing: '.1rem',
                             textDecoration: 'none',
-                            color: "inherit"
-                        }
-                    }}>
-                        Cosmic Planner
-                    </Typography>
+                            ":hover": {
+                                textDecoration: 'none',
+                                color: "inherit"
+                            }
+                        }}>
+                            Cosmic Planner
+                        </Typography>
+                        {navigations.map((navigation, index) => (
+                            <Button key={index} onClick={() => onNavButton(navigation.path)}>{navigation.name}</Button>
+                        ))}
+                    </Container>
+                    <Box>
+                        <Profile/>
+                    </Box>
                 </Toolbar>
             </Container>
         </AppBar>
