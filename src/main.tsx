898e80bba837f1447e9577ce7a301a7c052aa7ef
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-04-25 11:11:30 -0300

    fix: wrong Theme instance

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './ts/App.tsx'
 import './index.css'
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from "./ts/themes";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>,
 )
--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -1,15 +1,10 @@
 import './App.css'
-import {ThemeProvider} from "@mui/material";
-import {themeOptions} from "./themes";
 import {RouterProvider} from "react-router-dom";
 import router from "./Router";
 
 function App() {
-
     return (
-        <ThemeProvider theme={themeOptions}>
-            <RouterProvider router={router}/>
-        </ThemeProvider>
+        <RouterProvider router={router}/>
     )
 }
 
