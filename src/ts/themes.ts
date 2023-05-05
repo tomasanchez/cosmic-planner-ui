99830b1d78367ec8ad561f390e4ec2cd10375bae
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-05-05 15:08:41 -0300

    refactor(fonts): change to woff2

--- src/ts/themes.ts
+++ src/ts/themes.ts
@@ -1,13 +1,13 @@
 import {ThemeOptions} from '@mui/material/styles';
 import {amber, deepPurple, indigo, pink, red, teal} from "@mui/material/colors";
-import f72Black from "../fonts/72-Black.ttf";
-import f72Bold from "../fonts/72-Bold.ttf";
-import f72BoldItalic from "../fonts/72-BoldItalic.ttf";
-import f72Condensed from "../fonts/72-Condensed.ttf";
-import f72CondensedBold from "../fonts/72-CondensedBold.ttf";
-import f72Italic from "../fonts/72-Italic.ttf";
-import f72Light from "../fonts/72-Light.ttf";
-import f72Regular from "../fonts/72-Regular.ttf";
+import f72Black from "../fonts/72-Black.woff2";
+import f72Bold from "../fonts/72-Bold.woff2";
+import f72BoldItalic from "../fonts/72-BoldItalic.woff2";
+import f72Condensed from "../fonts/72-Condensed.woff2";
+import f72CondensedBold from "../fonts/72-CondensedBold.woff2";
+import f72Italic from "../fonts/72-Italic.woff2";
+import f72Light from "../fonts/72-Light.woff2";
+import f72Regular from "../fonts/72-Regular.woff2";
 import {createTheme} from "@mui/material";
 
 /**
@@ -20,57 +20,57 @@ const styleOverrides =
                 @font-face {
                     font-family: "72 Regular";
                     src: local("72 Regular"),
-                    url(${f72Regular}) format("truetype");
+                    url(${f72Regular}) format("woff2");
                 }
 
                 @font-face {
                     font-family: "72 Light";
                     src: local("72 Light"),
-                    url(${f72Light}) format("truetype");
+                    url(${f72Light}) format("woff2");
                 }
 
                 @font-face {
                     font-family: "72 Italic";
                     src: local("72 Italic"),
                     font-style: italic,
-                    url(${f72Italic}) format("truetype");
+                    url(${f72Italic}) format("woff2");
                 }
                 
                 @font-face {
                     font-family: "72 Condensed Bold";
                     src: local("72 Condensed Bold"),
-                    url(${f72CondensedBold}) format("truetype");
+                    url(${f72CondensedBold}) format("woff2");
                 }
                 
                 @font-face {
                     font-family: "72 Condensed";
                     src: local("72 Condensed"),
-                    url(${f72Condensed}) format("truetype");
+                    url(${f72Condensed}) format("woff2");
                 }
                 
                 @font-face {
                     font-family: "72 Bold Italic";
                     src: local("72 Bold Italic"),
                     font-style: italic,
-                    url(${f72BoldItalic}) format("truetype");
+                    url(${f72BoldItalic}) format("woff2");
                 }
                 
                 @font-face {
                     font-family: "72 Bold";
                     src: local("72 Bold "),
-                    url(${f72Bold}) format("truetype");
+                    url(${f72Bold}) format("woff2");
                 }
                 
                 @font-face {
                     font-family: "72 Black";
                     src: local("72 Black"),
-                    url(${f72Black}) format("truetype");
+                    url(${f72Black}) format("woff2");
                 }`
 ;
 
 const fontFamily = [
-    '-apple-system',
     '"72 Regular"',
+    '-apple-system',
     'BlinkMacSystemFont',
     '"Segoe UI"',
     'Roboto',
